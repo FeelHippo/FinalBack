5c7ffe848f89dc407f50b1a79655b9967b11b942
FeelHippo <mezzasegolas@gmail.com>
2020-06-17 22:35:32 +0200

    Let's Start!

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  if(err.array) { // validation error
+    err.status = 422;
+    const errInfo = err.array({ onlyFirstError: true })[0];
+    err.message = isAPIRequest(req) ?
+    { message: 'Not Valid', errors: err.mapped() }
+    : `Parameter ${errInfo.param} ${err.errInfo.msg}`
+  }
+  res.status(err.status || 500);
+
+  if(isAPIRequest(req)) {
+    res.json({ error: err.message });
+    return;
+  }
+
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.render('error');
+});
+
+function isAPIRequest(req) {
+  return req.originalUrl.startsWith('/api/');
+}
+
+module.exports = app;
